f933bf9d600c786db5d52bcd6bf030e5fcb59f47
Oluwadare Seyi <oluwadareseyii@gmail.com>
2021-07-08 19:00:02 +0100

    add SEO and some links

--- --hmr-hostname/scss.6b84482b.js
+++ --hmr-hostname/scss.6b84482b.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59035" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65435" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
